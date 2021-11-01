d12b3832ab5b7a592aa0c0b01190683b69d88990
Anam <anam.asif285@gmail.com>
2021-11-01 12:04:11 +0000

    added Routes

---
+++ src/LoginPage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Badge from 'react-bootstrap/Badge'
+
+function LoginPage() {
+    return (
+        
+    <>
+      <h1>Hello</h1>
+      <div>
+  <Badge bg="primary">Primary</Badge> <Badge bg="secondary">Secondary</Badge>{' '}
+  </div>
+  </>
+  );
+            
+   
+}
+
+export default LoginPage
